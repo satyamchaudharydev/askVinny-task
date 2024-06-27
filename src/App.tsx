abd9fd70b4ca87caf26e0b69aaa5805ef6d8051c
satyam_chaudhary <satyam.chaudhary@brightchamps.com>
2024-06-28 05:29:17 +0530

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 import "./styles.css";
 
 // Instructions for Candidate:
@@ -7,17 +10,64 @@ import "./styles.css";
 //the following url https://webhook.site/6064735c-c7f2-4584-ba9d-1f0e80f32721. Along with the email, send your github username in the JSON. 
 // 4. Add styling to the button (Button) and input (Input) using the ShadCN Component library here: https://ui.shadcn.com/docs/components/input
 
+const URL = "https://webhook.site/6064735c-c7f2-4584-ba9d-1f0e80f32721"
+const githubUsername = "satyamchaudharydev"
 
-const EmailForm = () => {
-  return <div>Email form</div>;
+const EmailForm = ({
+  onSubmit
+}: {
+  onSubmit: (email: string) => void
+}) => {
+  const [email, setEmail] = useState<string>("");
+  
+  const handleSubmit = () => {
+    onSubmit(email)
+  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const email = e.target.value
+      setEmail(email)
+  }
+  return <div>Email form
+    <form onSubmit={handleSubmit}>
+      <Input type="email" name="email" value={email} onChange={handleChange} />
+      <Button type="submit"  className="mt-3" >
+        Submit
+      </Button>
+    </form>
+    
+  </div>;
 };
 
 export default function App() {
+
+  const onSubmit = async (email: string) => {
+    const data = {
+      email,
+      githubUsername
+    }
+    try {
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      if(!response.ok){
+        throw new Error("Error is present")
+      }
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+   
+  }
   return (
-    <div className="App">
+    <div className="App flex justify-center items-center flex-col h-full">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <EmailForm />
+    
+      <EmailForm onSubmit={onSubmit} />
     </div>
   );
 }
